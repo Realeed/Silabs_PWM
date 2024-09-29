36a4951ac85e7ae5b0b0705bdc2c38ab1f118071
Realeed <73318913+Realeed@users.noreply.github.com>
2024-09-29 06:55:11 +0400

    Add files via upload

---
+++ pwm.c
@@ -0,0 +1,131 @@
+/*
+ * pwm.c
+ *
+ *  Created on: Sep 28, 2024
+ *      Author: HS
+ */
+
+#include "pwm.h"
+
+bool timerInitialized[11] = {
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+};
+
+PWM_RETURN_E pwm_setDutyCycle(TIMER_TypeDef *timer, uint8_t ch, uint16_t dutyCycle1000)
+{
+  if (!timerInitialized[TIMER_DEVICE_ID(timer)]) {
+      return PWM_TIMER_NOT_INITIALIZED;
+  }
+  if (dutyCycle1000 > 1000) {
+      return PWM_INVALID_PARAM;
+  }
+  uint32_t top = TIMER_TopGet(timer);
+  uint32_t compare = (top+1) * dutyCycle1000 / 1000;
+  TIMER_CompareSet(timer, ch, compare);
+
+  return PWM_DONE;
+}
+
+PWM_RETURN_E pwm_setPwmFreq(TIMER_TypeDef *timer, uint32_t pwmFreq)
+{
+  if (!timerInitialized[TIMER_DEVICE_ID(timer)]) {
+      return PWM_TIMER_NOT_INITIALIZED;
+  }
+  if (pwmFreq < 650) {
+      return PWM_INVALID_PARAM;
+  }
+  CMU_Clock_TypeDef clock = TIMER_2_CMU_CLOCK(timer);
+  uint32_t clockFreq = CMU_ClockFreqGet(clock);
+  uint32_t currentTop = TIMER_TopGet(timer);
+  uint32_t top = clockFreq / pwmFreq - 1;
+  TIMER_TopSet(timer, top);
+
+  for (int i = 0; i < 3; i++) {
+      uint32_t dutyCycle1000 = timer->CC[i].OC * 1000 / (currentTop + 1);
+      pwm_setDutyCycle(timer, i, dutyCycle1000);
+  }
+
+  return PWM_DONE;
+}
+
+PWM_RETURN_E pwm_start(TIMER_TypeDef *timer)
+{
+  if (!timerInitialized[TIMER_DEVICE_ID(timer)]) {
+      return PWM_TIMER_NOT_INITIALIZED;
+  }
+  TIMER_Enable(timer, true);
+
+  return PWM_DONE;
+}
+
+PWM_RETURN_E pwm_stop(TIMER_TypeDef *timer)
+{
+  if (!timerInitialized[TIMER_DEVICE_ID(timer)]) {
+      return PWM_TIMER_NOT_INITIALIZED;
+  }
+  TIMER_Enable(timer, false);
+  TIMER_CounterSet(timer, _TIMER_CNT_RESETVALUE);
+
+  return PWM_DONE;
+}
+
+PWM_RETURN_E pwm_init(TIMER_TypeDef *timer, PWM_INIT_S *pwmInit)
+{
+  if ((pwmInit->numOfChsUsed > 3) || (pwmInit->numOfChsUsed < 1)
+      || (pwmInit->pwmFreq < 650)
+      || (pwmInit->ch[0].dutyCycle1000 > 1000)
+      || (pwmInit->ch[1].dutyCycle1000 > 1000)
+      || (pwmInit->ch[2].dutyCycle1000 > 1000)) {
+      return PWM_INVALID_PARAM;
+  }
+
+  uint8_t timerID = TIMER_DEVICE_ID(timer);
+
+  for (int i = 0; i < pwmInit->numOfChsUsed; i++) {
+      GPIO_PinModeSet(pwmInit->ch[i].port, pwmInit->ch[i].pin, gpioModePushPull, 0);
+      GPIO->TIMERROUTE_SET[timerID].ROUTEEN = (1UL << i);
+  }
+
+  switch (pwmInit->numOfChsUsed) {
+    case 1:
+     GPIO->TIMERROUTE_SET[timerID].CC0ROUTE = ((pwmInit->ch[0].pin << 16) | pwmInit->ch[0].port);
+     break;
+    case 2:
+     GPIO->TIMERROUTE_SET[timerID].CC0ROUTE = ((pwmInit->ch[0].pin << 16) | pwmInit->ch[0].port);
+     GPIO->TIMERROUTE_SET[timerID].CC1ROUTE = ((pwmInit->ch[1].pin << 16) | pwmInit->ch[1].port);
+     break;
+    case 3:
+     GPIO->TIMERROUTE_SET[timerID].CC0ROUTE = ((pwmInit->ch[0].pin << 16) | pwmInit->ch[0].port);
+     GPIO->TIMERROUTE_SET[timerID].CC1ROUTE = ((pwmInit->ch[1].pin << 16) | pwmInit->ch[1].port);
+     GPIO->TIMERROUTE_SET[timerID].CC2ROUTE = ((pwmInit->ch[2].pin << 16) | pwmInit->ch[2].port);
+     break;
+  }
+
+  TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
+  timerInit.enable = false;
+  TIMER_Init(timer, &timerInit);
+
+  TIMER_InitCC_TypeDef ccInit = TIMER_INITCC_DEFAULT;
+  ccInit.mode = timerCCModePWM;
+  for (int i = 0; i < pwmInit->numOfChsUsed; i++) {
+      TIMER_InitCC(timer, i, &ccInit);
+  }
+  timerInitialized[timerID] = true;
+  pwm_setPwmFreq(timer, pwmInit->pwmFreq);
+
+  for (int i = 0; i < pwmInit->numOfChsUsed; i++) {
+      pwm_setDutyCycle(timer, i, pwmInit->ch[i].dutyCycle1000);
+  }
+
+  return PWM_DONE;
+}
---
+++ pwm.h
@@ -0,0 +1,123 @@
+/*
+ * pwm.h
+ *
+ *  Created on: Sep 28, 2024
+ *      Author: HS
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include "em_cmu.h"
+#include "em_gpio.h"
+#include "em_timer.h"
+
+/** Map TIMER reference to index of device and CMU Clock. */
+#if defined(TIMER9)
+#define TIMER_DEVICE_ID(timer) ( \
+    (timer) == TIMER0     ? 0    \
+    : (timer) == TIMER1   ? 1    \
+    : (timer) == TIMER2   ? 2    \
+    : (timer) == TIMER3   ? 3    \
+    : (timer) == TIMER4   ? 4    \
+    : (timer) == TIMER5   ? 5    \
+    : (timer) == TIMER6   ? 6    \
+    : (timer) == TIMER7   ? 7    \
+    : (timer) == TIMER8   ? 8    \
+    : (timer) == TIMER9   ? 9    \
+    : 10)
+#define TIMER_2_CMU_CLOCK(timer) ( \
+    (timer) == TIMER0     ? cmuClock_TIMER0    \
+    : (timer) == TIMER1   ? cmuClock_TIMER1    \
+    : (timer) == TIMER2   ? cmuClock_TIMER2    \
+    : (timer) == TIMER3   ? cmuClock_TIMER3    \
+    : (timer) == TIMER4   ? cmuClock_TIMER4    \
+    : (timer) == TIMER5   ? cmuClock_TIMER5    \
+    : (timer) == TIMER6   ? cmuClock_TIMER6    \
+    : (timer) == TIMER7   ? cmuClock_TIMER7    \
+    : (timer) == TIMER8   ? cmuClock_TIMER8    \
+    : (timer) == TIMER9   ? cmuClock_TIMER9    \
+    : 10)
+#elif defined(TIMER7)
+#define TIMER_DEVICE_ID(timer) ( \
+    (timer) == TIMER0     ? 0    \
+    : (timer) == TIMER1   ? 1    \
+    : (timer) == TIMER2   ? 2    \
+    : (timer) == TIMER3   ? 3    \
+    : (timer) == TIMER4   ? 4    \
+    : (timer) == TIMER5   ? 5    \
+    : (timer) == TIMER6   ? 6    \
+    : (timer) == TIMER7   ? 7    \
+    : 10)
+#define TIMER_2_CMU_CLOCK(timer) ( \
+    (timer) == TIMER0     ? cmuClock_TIMER0    \
+    : (timer) == TIMER1   ? cmuClock_TIMER1    \
+    : (timer) == TIMER2   ? cmuClock_TIMER2    \
+    : (timer) == TIMER3   ? cmuClock_TIMER3    \
+    : (timer) == TIMER4   ? cmuClock_TIMER4    \
+    : (timer) == TIMER5   ? cmuClock_TIMER5    \
+    : (timer) == TIMER6   ? cmuClock_TIMER6    \
+    : (timer) == TIMER7   ? cmuClock_TIMER7    \
+    : 10)
+#elif defined(TIMER4)
+#define TIMER_DEVICE_ID(timer) ( \
+    (timer) == TIMER0   ? 0      \
+    : (timer) == TIMER1 ? 1      \
+    : (timer) == TIMER2 ? 2      \
+    : (timer) == TIMER3 ? 3      \
+    : (timer) == TIMER4 ? 4      \
+    : 10)
+#define TIMER_2_CMU_CLOCK(timer) ( \
+    (timer) == TIMER0   ? cmuClock_TIMER0      \
+    : (timer) == TIMER1 ? cmuClock_TIMER1      \
+    : (timer) == TIMER2 ? cmuClock_TIMER2      \
+    : (timer) == TIMER3 ? cmuClock_TIMER3      \
+    : (timer) == TIMER4 ? cmuClock_TIMER4      \
+    : 10)
+#else
+#define TIMER_DEVICE_ID(timer) ( \
+    (timer) == TIMER0   ? 0      \
+    : (timer) == TIMER1 ? 1      \
+    : (timer) == TIMER2 ? 2      \
+    : (timer) == TIMER3 ? 3      \
+    : 10)
+#define TIMER_2_CMU_CLOCK(timer) ( \
+    (timer) == TIMER0   ? cmuClock_TIMER0      \
+    : (timer) == TIMER1 ? cmuClock_TIMER1      \
+    : (timer) == TIMER2 ? cmuClock_TIMER2      \
+    : (timer) == TIMER3 ? cmuClock_TIMER3      \
+    : 10)
+#endif
+
+typedef enum
+{
+  PWM_DONE = 0,
+  PWM_INVALID_PARAM = -1,
+  PWM_TIMER_NOT_INITIALIZED = -2,
+
+} PWM_RETURN_E;
+
+typedef struct
+{
+    GPIO_Port_TypeDef port;
+    uint8_t pin;
+    uint16_t dutyCycle1000;
+
+} CC_CH_S;
+
+typedef struct
+{
+    uint32_t pwmFreq;
+    uint8_t numOfChsUsed;
+    CC_CH_S ch[3];
+
+} PWM_INIT_S;
+
+PWM_RETURN_E pwm_init(TIMER_TypeDef *timer, PWM_INIT_S *pwmInit);
+PWM_RETURN_E pwm_start(TIMER_TypeDef *timer);
+PWM_RETURN_E pwm_stop(TIMER_TypeDef *timer);
+PWM_RETURN_E pwm_setPwmFreq(TIMER_TypeDef *timer, uint32_t pwmFreq);
+PWM_RETURN_E pwm_setDutyCycle(TIMER_TypeDef *timer, uint8_t ch, uint16_t dutyCycle1000);
+
+
+#endif /* PWM_H_ */
